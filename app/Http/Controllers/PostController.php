a11f5d04762f1e9e6de328fa14f73817b6806b86
lloricode <emorickfighter@gmail.com>
2017-06-26 03:42:15 +0800

    read our CRUD - video 16
    Signed-off-by: lloricode <emorickfighter@gmail.com>

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,10 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        //create a variable and store all the blog post in it from the datacase
+        $posts=Post::all();
+        //return a view and post in the above variable
+        return view('posts.index')->withPosts($posts);
     }
 
     /**
