cd44af393836c85a7e01d87278a6ff74b623d022
lloricode <emorickfighter@gmail.com>
2017-06-27 22:50:51 +0800

    slugs in our url routes - video 24
    Signed-off-by: lloricode <emorickfighter@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,16 @@ Route::get('/', 'PagesController@getIndex');
 Route::get('/about', 'PagesController@getAbout');
 Route::get('/contact', 'PagesController@getContact');
 
+Route::get('blog/{slug}',['as'=>'blog.single','uses'=>'BlogController@getSingle'])
+/*
+
+ \w - any letter character
+ \d - any number character
+ \- - -
+ \_ - _
+ + - how many
+
+ */ 
+->where('slug','[\w\d\-\_]+');
+
 Route::resource('posts','PostController');
